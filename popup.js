e5d5abafe2b407495e6203e9d2797c311746dbab
Ryan Delaney <rrdelaney@outlook.com>
2015-09-12 14:13:34 -0400

    I hate chrome

--- popup.js
+++ popup.js
@@ -1,29 +1,10 @@
 $(function() {
-    $('#candidate-reveal').click(function() {
-        $('#like-container').hide();
-        Politik.candidate.getCandidate(.3, function(candidate) {
-            Politik.ui.getCandidateCard(candidate, function(card) {
-                $('#candidate-container').html(card);
-                $('#candidate-link').click(function() {
-                    Politik.ui.openTab($('#candidate-link').attr('href'));
-                });
+    Politik.candidate.getCandidate(.3, function(candidate) {
+        Politik.ui.getCandidateCard(candidate, function(card) {
+            $('#candidate-container').html(card);
+            $('#candidate-link').click(function() {
+                Politik.ui.openTab($('#candidate-link').attr('href'));
             });
         });
     });
-
-    $('#like-button').click(function() {
-        Politik.location.getCurrent(function(tab) {
-            Politik.page.send(tab, 1, function() {
-                console.log(tab.url + " liked!");
-            });
-        });
-    });
-
-    $('#dislike-button').click(function() {
-        Politik.location.getCurrent(function(tab) {
-            Politik.page.send(tab, 0, function() {
-                console.log(tab.url + "disliked!");
-            });
-        })
-    })
 });
