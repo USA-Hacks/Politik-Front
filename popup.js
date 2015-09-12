c5ebc591ecff7e6c6be3cb11df5adbf1ac576c87
Ryan Delaney <rrdelaney@outlook.com>
2015-09-12 15:59:50 -0400

    New logo

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 $(function() {
-    Politik.candidate.getCandidate(-.6, function(candidate) {
+    Politik.candidate.getCandidate(1, function(candidate) {
         Politik.ui.getCandidateCard(candidate, function(card) {
             $('#candidate-container').html(card);
             $('#candidate-link').click(function() {
